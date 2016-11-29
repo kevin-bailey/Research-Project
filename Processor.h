23c4eacaa5ebce9ebec4ed36c5e6bebc75e34a4d
kevin-bailey <kbailey600@yahoo.com>
2016-11-29 11:39:23 -0500

    Used to measure CPU time

---
+++ Processor.h
@@ -0,0 +1,16 @@
+#pragma once
+/*
+* Author of getCPUTime():  David Robert Nadeau
+* Site:    http://NadeauSoftware.com/
+* License: Creative Commons Attribution 3.0 Unported License
+*          http://creativecommons.org/licenses/by/3.0/deed.en_US
+*/
+class Processor
+{
+public:
+	Processor();
+	double getCPUTime();
+
+	~Processor();
+};
+
