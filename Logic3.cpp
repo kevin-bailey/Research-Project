5b20d84fbb9d0b94a7c89ff56e597ef0123609a2
kevin-bailey <kbailey600@yahoo.com>
2016-11-30 01:34:50 -0500

    Update Logic3.cpp

--- Logic3.cpp
+++ Logic3.cpp
@@ -13,47 +13,62 @@ int Logic3::KAND(int a, int b) {
 	// Kleene AND
 	// Returns FALSE if any trit is FALSE
 	// Returns TRUE if both trits are TRUE
-	/* 0 0 0
+	/* 
+	0 0 0
 	0 1 1
 	0 1 2
 	*/
-	if (a == FALSE || b == FALSE) return FALSE;
-	if (a == UNCERTAIN || b == UNCERTAIN) return UNCERTAIN;
+	if (a == FALSE) return FALSE;
+	if (b == FALSE) return FALSE;
+	if (a == UNCERTAIN) return UNCERTAIN;
+	if (b == UNCERTAIN) return UNCERTAIN;
 	return TRUE;
 }
 
 int Logic3::KOR(int a, int b) {
 	// Kleene OR
-	// Returns TRUE if any trit is TRIE
+	// Returns TRUE if any trit is TRUE
 	// Returns FALSE if both trits are FALSE
-	/* 0 1 2
+	/* 
+	0 1 2
 	1 1 2
 	2 2 2
 	*/
-	if (a == TRUE || b == TRUE) return TRUE;
-	if (a == UNCERTAIN || b == UNCERTAIN) return UNCERTAIN;
+	if (a == TRUE) return TRUE;
+	if (b == TRUE) return TRUE;
+	if (a == UNCERTAIN) return UNCERTAIN;
+	if (b == UNCERTAIN) return UNCERTAIN;
 	return FALSE;
 }
 
 int Logic3::TAND(int a, int b) {
 	// Ternary NAND
-	// Returns UNCERTAIN if any trit is FALSE or UNCERTAIN
-	/* 1 1 1
+	// Returns UNCERTAIN if any trit is FALSE
+	/* 
+	1 1 1
 	1 0 0
 	1 0 0
+
+	Note: this is actually not part of the ternary adder right now,
+	but it is still present in the logic system used to construct it.
+	if anyone thinks they can improve upon the adder using it, please do!
 	*/
-	if (a == FALSE || b == FALSE) return UNCERTAIN;
+	if (a == FALSE) return UNCERTAIN;
+	if (b == FALSE) return UNCERTAIN;
 	return FALSE;
 }
 
 int Logic3::TOR(int a, int b) {
 	// Ternary NOR
-	// Returns UNCERTAIN if any trit is TRUE or UNCERTAIN
-	/* 0 0 1
+	// Returns UNCERTAIN if any trit is TRUE
+	/* 
+	0 0 1
 	0 0 1
 	1 1 1
 	*/
-	if (a == TRUE || b == TRUE) return UNCERTAIN;
+
+	if (a == TRUE) return UNCERTAIN;
+	if (b == TRUE) return UNCERTAIN;
 	return FALSE;
 }
 
@@ -61,11 +76,13 @@ int Logic3::BAND(int a, int b) {
 	// Binary NAND
 	// Returns TRUE if any trit is FALSE
 	// Otherwise return FALSE
-	/* 2 2 2
+	/* 
+	2 2 2
 	2 0 0
 	2 0 0
 	*/
-	if (a == FALSE || b == FALSE) return TRUE;
+	if (a == FALSE) return TRUE;
+	if (b == FALSE) return TRUE;
 	return FALSE;
 }
 
@@ -73,11 +90,13 @@ int Logic3::BOR(int a, int b) {
 	// Binary NOR
 	// Returns TRUE if any trit is TRUE
 	// Otherwise return FALSE
-	/* 0 0 2
+	/* 
+	0 0 2
 	0 0 2
 	2 2 2
 	*/
-	if (a == TRUE || b == TRUE) return TRUE;
+	if (a == TRUE) return TRUE;
+	if (b == TRUE) return TRUE;
 	return FALSE;
 }
 
@@ -89,7 +108,8 @@ int Logic3::NOT(int a) {
 
 int Logic3::REM(int a, int b) {
 	// This "gate" calculates the remainder of 2 trits
-	/* 0 0 0
+	/* 
+	0 0 0
 	0 0 1
 	0 1 1
 	*/
@@ -98,7 +118,8 @@ int Logic3::REM(int a, int b) {
 
 int Logic3::SUM(int a, int b) {
 	// This "gate" calculates the sum of two trits
-	/* 0 1 2
+	/* 
+	0 1 2
 	1 2 0
 	2 0 1
 	*/
@@ -108,7 +129,8 @@ int Logic3::SUM(int a, int b) {
 int Logic3::CTRL(int a, int b) {
 	// This "gate" is used nothing more as a catalyst for our adder
 	// In truth, we don't even need this method, but it will look neater in the code.
-	/* 2 2 2
+	/* 
+	2 2 2
 	2 2 0
 	2 0 2
 	*/
@@ -116,19 +138,8 @@ int Logic3::CTRL(int a, int b) {
 }
 
 void Logic3::ADD(int a, int b, int cin, int& cout, int& sum) {
-	/* cout = SUM(a, b);
-	sum = SUM(cout, cin);
-	cout = REM(a, b); */
-
 	sum = SUM(cin, SUM(a, b));
 	cout = KOR(REM(a, b), REM(cin, SUM(a, b)));
-}
-
-int Logic3::ternaryConversion(int decimalNum) {
-	if (decimalNum <= 2) return decimalNum;
-	int carry = decimalNum % 3;
-	ternaryConversion(decimalNum >> 2);
-	return carry;
 }
 
 Logic3::~Logic3()
